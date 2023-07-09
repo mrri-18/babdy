c40f328d8e822ce137fc33d25065dae57c6d4f91
leemk <pallang719@naver.com>
2023-07-09 23:55:45 +0900

    lesson17 post text

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,11 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
+from accountapp.models import Helloworld
 def hello_world(request):
+
     if request.method == "POST": #POST METHOD를 요청받을 경우
+         temp = request.POST.get('hello_world_input')  # hello_world_input 변수의 데이터를 가져와라
+
+         new_hello_world=Helloworld()
+         new_hello_world.text=temp
+         new_hello_world.save() #db에 객체가 저장됨.
+
          # 텍스트라는 이름에 내용은 POST METHOD! CONTEXT= 데이터 꾸러미, post method가 뜬다= 정상적으로 post 요청을 보내고 응답을 받은 것
-         return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!'}) #request에 대한 응답
+         return render(request, 'accountapp/hello_world.html', context={'hello_world_output':new_hello_world}) #request에 대한 응답
     else:
         return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD!'})
 
