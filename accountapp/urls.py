326a58c066f8e780e31b2156637c8ad816530cd9
leemk <pallang719@naver.com>
2023-07-11 23:03:53 +0900

    lesson22 login redirect

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path,include
 
 from accountapp.views import hello_world, AccountCreateView
@@ -6,4 +7,6 @@ app_name="accountapp" #지정-> hello_wolrd 경로로 접근할 때
 urlpatterns = [
     path('hello_world/',hello_world,name='hello_world'), # account 하위 주소인 hello_world 주소로 접근하면 view를 보여주겠다 함수 기반은 그냥 함수명만 써줘도 괜찮
     path('create/',AccountCreateView.as_view(),name='create'), #클래스 기반은 함수와 함께
+    path('login/',LoginView.as_view(template_name='accountapp/login.html'),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
 ]
--- babdmay/settings.py
+++ babdmay/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -126,3 +128,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL=reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL=reverse_lazy('accountapp:login')
