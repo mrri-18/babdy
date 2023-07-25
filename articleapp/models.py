f5540a95f34af39a156de6ced6b59898e9bb06a9
leemk <pallang719@naver.com>
2023-07-25 22:54:41 +0900

    articleapp complete

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    writer=models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True) #user 객체의 아티클에 접근할 때 writer 사용.
+    title=models.CharField(max_length=200, null=True)
+    image=models.ImageField(upload_to='article/', null=False)
+    content=models.TextField(null=True) #긴 내용
+    created_at=models.DateField(auto_created=True, null=True)
