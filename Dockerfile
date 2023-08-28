76a53b7651185595909707e85dc014edae572752
leemk <pallang719@naver.com>
2023-08-28 23:29:56 +0900

    db 설정

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM python:3.9.0
 
 WORKDIR /home/
 
+RUN echo "testing"
+
 RUN git clone https://github.com/mrri-18/babdy.git
 
 WORKDIR /home/babdy/
@@ -11,13 +13,13 @@ RUN pip install -r requirements.txt
 
 RUN pip install gunicorn
 
-RUN echo "SECRET_KEY=django-insecure-w%rd#b%^86a5&dre9)wal^$f(gy&qm_i3zzd8hu+79+l!v300i" > .env
+RUN pip install mysqlclient
 
-RUN python manage.py migrate
+RUN echo "SECRET_KEY=django-insecure-w%rd#b%^86a5&dre9)wal^$f(gy&qm_i3zzd8hu+79+l!v300i" > .env
 
 RUN echo yes | python manage.py collectstatic
 
 EXPOSE 8000
 
-CMD ["gunicorn","babdmay.wsgi","--bind","0.0.0.0:8000"]
+CMD ["bash","-c","python manage.py migrate --settings=babdmay.settings.deploy && gunicorn babdmay.wsgi --env DJANGO_SETTINGS_MODULE=babdmay.settings.deploy --bind 0.0.0.0:8000"]
 
--- babdmay/settings/base.py
+++ babdmay/settings/base.py
@@ -16,24 +16,12 @@ import environ
 import os
 
 from django.urls import reverse_lazy
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -83,15 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'babdmay.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
